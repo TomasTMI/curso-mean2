de31197fcb5ae90ffcd771abdd8e252910bd0824
Tomas Martinez <tmartinez@tmi-sl.com>
2017-05-10 19:53:38 +0200

    Clase 19. Function/Route Login

--- controllers/user.js
+++ controllers/user.js
@@ -43,7 +43,38 @@ function saveUser(req, res) {
     }
 }
 
+// ToDo: toLowerCase (email); Si falta email, control.
+function loginUser(req, res) {
+    var params = req.body;
+
+    var email = params.email; //.toLowerCase();
+    var password = params.password;
+
+    User.findOne({email: email}, (err, user) => {
+        if (err) {
+            res.status(500).send({message: 'Error en la petición'});
+        } else {
+            if (!user) {
+                res.status(404).send({message: 'El usuario no existe'});
+            } else {
+                bcrypt.compare(password, user.password, (err, check) => {
+                    if (check) {
+                        if (params.gethash) {
+                            //...
+                        } else {
+                            res.status(200).send({user});
+                        }
+                    } else {
+                        res.status(404).send({message: 'El usuario no ha podido loguearse'});
+                    }
+                });
+            }
+        }
+    });
+}
+
 module.exports = {
     pruebas,
-    saveUser
+    saveUser,
+    loginUser
 };
