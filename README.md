199f95af3232cd954b545d58621383f7ef9d2d2b
Tomas Martinez <tmartinez@tmi-sl.com>
2017-05-10 19:09:33 +0200

    Clase 17. Register

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Curso-mean2
 
 Curso:
-Desarrollo web con JavaScript, Angular, NodeJS y MongoDB
+Desarrollo web con JavaScript, Angular, NodeJS y MongoDB.
 Mini-Spotify
--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,49 @@
 /*jshint esversion: 6*/
 
+var bcrypt = require('bcrypt-nodejs');
+var User = require('../models/user');
 
 function pruebas(req, res) {
     res.status(200).send({message: 'Acción controlador'});
 }
 
+function saveUser(req, res) {
+    var user = new User();
+    var params = req.body;
+
+    console.log(params);
+
+    user.name = params.name;
+    user.surname = params.surname;
+    user.email = params.email;
+    user.role = 'ROLE_USER';
+    user.image = 'null';
+
+    if(params.password) {
+        bcrypt.hash(params.password, null, null, (err, hash) =>{
+            user.password = hash;
+            if (user.name != null && user.surname != null && user.email != null) {
+                user.save((err, userStored) => {
+                    if (err) {
+                        res.status(500).send({message: 'Error al guardar al usuario'});
+                    } else {
+                        if (!userStored) {
+                            res.status(404).send({message: 'No se ha registrado el usuario'});
+                        } else {
+                            res.status(200).send({User: userStored});
+                        }
+                    }
+                });
+            } else {
+                res.status(200).send({message: 'Rellena todos los campos'});
+            }
+        });
+    } else {
+        res.status(500).send({message: 'Introduce la contraseña'});
+    }
+}
+
 module.exports = {
-    pruebas
+    pruebas,
+    saveUser
 };
