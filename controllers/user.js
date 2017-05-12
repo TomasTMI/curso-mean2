0f681542d077e2cff4540230487bd05ffe3ae3cd
Tomas Martinez <tmartinez@tmi-sl.com>
2017-05-12 11:42:56 +0200

    Clase22...(bis)

--- controllers/user.js
+++ controllers/user.js
@@ -92,9 +92,40 @@ function updateUser(req, res) {
     });
 }
 
+function uploadImage(req, res) {
+    var userId = req.params.id;
+    var file_name = 'No subido...';
+
+    if (req.files) {
+        var file_path = req.files.image.path;
+        var file_split = file_path.split('\\');
+        file_name = file_split[2];
+        var ext_split = file_name.split('\.');
+        var file_ext = ext_split[1].toLowerCase();
+        if (file_ext=='png' || file_ext=='jpg' || file_ext=='gif') {
+            User.findByIdAndUpdate(userId, {image: file_name}, (err, userUpdated) => {
+                if (err) {
+                    res.status(500).send({message: 'Error al actualizar al usuario'});
+                } else {
+                    if (!userUpdated) {
+                        res.status(404).send({message: 'No se ha podido actualizar al usuario'});
+                    } else {
+                        res.status(200).send({user: userUpdated});
+                    }
+                }
+            });
+        } else {
+            res.status(200).send({message: 'Extensión del archivo no válido'});
+        }
+    } else {
+        res.status(200).send({message: 'No has subido nunguna imagen...'});
+    }
+}
+
 module.exports = {
     pruebas,
     saveUser,
     loginUser,
-    updateUser
+    updateUser,
+    uploadImage
 };
